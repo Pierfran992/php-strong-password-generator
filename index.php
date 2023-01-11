c6f4cd14504a342428b1303f285a3555685553c0
Pierfran992 <santoro.pierfrancesco@hotmail.it>
2023-01-11 15:31:21 +0100

    Sviluppo: - Creato il form per inviare tramite get la lunghezza della password; -Creata la funzione per generare una password casuale in base alla lunghezza che gli è stata passata dal form;

--- index.php
+++ index.php
@@ -5,10 +5,50 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Generator Strong Password</title>
+
+    <style>
+        .container {
+            width: 900px;
+            margin: 50px auto;
+            text-align: center;
+        }
+    </style>
+
+    <?php
+        // creo il paramentro in get per inviare la lunghezza della password
+        $length = $_GET['length'];
+
+        // creo la funzione per generare una password
+        function genPassword($length) {
+            $characters = array_merge(range('A', 'Z'), range('a', 'z'), range('0', '9'), array('!', '@', '#', '$', '%', '^', '&', '*'));
+            $password = "";
+            for ($i = 0; $i < $length; $i++) {
+                $password .= $characters[array_rand($characters)];
+            }
+            return $password;
+        }
+    ?>
 </head>
 <body>
-    <?php
-    echo "hello world";
-    ?>
+    <div class="container">
+        <h1>Strong Password Generator</h1>
+        <h2>Genera una password sicura</h2>
+
+        <!-- form per inviare tramite get la lunghezza della password -->
+        <form method="get">
+            <label for="pswLength">Scegli la lunghezza della password:</label>
+            <input type="number" name="length" id="pswLength">
+            <input type="submit" value="Crea">
+        </form>
+
+        <!-- container in cui stampare la password -->
+        <div class="ctn_psw">
+            <span>
+                <?php
+                echo genPassword($length);
+                ?>
+            </span>
+        </div>
+    </div>
 </body>
 </html>
